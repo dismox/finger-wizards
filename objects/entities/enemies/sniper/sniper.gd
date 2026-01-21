23ac178179c2304e2b422141240910364b56c20d
dismox <dismoxem@gmail.com>
2026-01-21 19:06:22 +0700

    доделал снипера

--- objects/entities/enemies/sniper/sniper.gd
+++ objects/entities/enemies/sniper/sniper.gd
@@ -2,8 +2,17 @@ extends Enemy
 class_name Sniper
 
 func attack(dir: Vector2) -> void:
-	await get_tree().create_timer(0.2).timeout
-	shoot(dir)
+	#await get_tree().create_timer(0.2).timeout
+	var default_bullet_range = bullet_range
+	#var default_bullet_speed = bullet_speed
+	bullet_range = 10.0
+	#bullet_speed = 300.0
+	while bullet_range < default_bullet_range:
+		await get_tree().create_timer(0.01).timeout
+		shoot(dir)
+		bullet_range += 10
+		#bullet_speed -= 10
+	
 
 func apply_damage(amount: float) -> void:
 	Game.spawn_paddle(global_position, Game.Puddle_type.RED, Vector2(0.5, scale.x))
